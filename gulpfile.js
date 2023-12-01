6f995fec3f26556e416fe3ccf92d71412840686a
Quiatus <Quiatus@gmail.com>
2023-12-01 19:02:39 +0100

    fix sass folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('sass/style.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
